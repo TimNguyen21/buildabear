ecbd5dafa50583e109782c9584f6ad9cb65afbef
John Watterson <john@breakthrewbrew.com>
2019-12-18 14:06:27 -0700

    Add new function to change button states - function does NOT currently work

--- main.js
+++ main.js
@@ -9,23 +9,38 @@ var outfit = new Outfit(id, null, [], null);
 
 // event listener on options selection
 hats.addEventListener('click', addHats);
+
+// hats.addEventListener('click', changeButtonToActive);
+// hats.addEventListener('click', function() {
+//   changeButtonToActive();
+//   console.log('is the change button state working')
+//   addHats();
+// });
+
 clothes.addEventListener('click', addClothes);
 accessories.addEventListener('click', addAccessories);
 background.addEventListener('click', addBackground);
 
 // add hats
 
-function addHats(e){
+function addHats(e) {
   // var lastClicked =
   // if (e.target != e.currentTarget) {
   if (e.target.classList.contains('top-hat')) {
-    outfit.addGarment('top hat');
+    outfit.addGarment('top-hat');
   } else if (e.target.classList.contains('sun-hat')) {
-    outfit.addGarment('sun hat');
+    outfit.addGarment('sun-hat');
   } else if (e.target.classList.contains('bow')) {
     outfit.addGarment('bow');
   } else if (e.target.classList.contains('crown')) {
     outfit.addGarment('crown');
+  }
+}
+
+function changeButtonToActive(event) {
+  if (event.target.classList.contains('.hat-options-js')) {
+    console.log('turning pink button to active');
+    event.target.closest('.hat-options-js').classList.add('.pink-button-active');
   }
 }
 
