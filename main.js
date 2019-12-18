b0cf937f88f1ea98b3ba641d053fc28bcc5ed073
Justin Corbin <jcorbin2290@gmail.com>
2019-12-18 16:36:40 -0700

    OOPS was on master

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var outfit = new Outfit(id, null, [], null);
 // event listener on options selection
 hats.addEventListener('click', addHats);
 
-// hats.addEventListener('click', changeButtonToActive);
+hats.addEventListener('click', changeButtonToActive);
 // hats.addEventListener('click', function() {
 //   changeButtonToActive();
 //   console.log('is the change button state working')
@@ -20,40 +20,34 @@ clothes.addEventListener('click', addClothes);
 accessories.addEventListener('click', addAccessories);
 background.addEventListener('click', addBackground);
 
+function changeButtonToActive(event) {
+  if (event.target.classList.contains('hat-options-js')) {
+    event.target.closest('.hat-options-js').classList.add('pink-button-active');
+    console.log(1);
+  }
+}
+
 // add hats
 function addHats(e){
   if (e.target.classList.contains('top-hat')) {
-    console.log(1);
     outfit.addGarment(0, 'top-hat');
   } else if (e.target.classList.contains('sun-hat')) {
-    console.log(2);
     outfit.addGarment(0, 'sun-hat');
   } else if (e.target.classList.contains('bow')) {
-    console.log(3);
     outfit.addGarment(0, 'bow');
   } else if (e.target.classList.contains('crown')) {
-    console.log(4);
     outfit.addGarment(0, 'crown');
   }
 }
 
-function changeButtonToActive(event) {
-  if (event.target.classList.contains('.hat-options-js')) {
-    console.log('turning pink button to active');
-    event.target.closest('.hat-options-js').classList.add('.pink-button-active');
-  }
-}
 
 // add clothes
 function addClothes(e) {
   if (e.target.classList.contains('t-shirt')) {
-    console.log(1);
     outfit.addGarment(1, 't-shirt');
   } else if (e.target.classList.contains('dress')) {
-    console.log(2);
     outfit.addGarment(1, 'dress');
   } else if (e.target.classList.contains('tuxedo')) {
-    console.log(3);
     outfit.addGarment(1, 'tuxedo');
   }
 }
@@ -61,19 +55,14 @@ function addClothes(e) {
 // add accessories
 function addAccessories(e) {
   if (e.target.classList.contains('necklace')) {
-    console.log(1);
     outfit.addGarment(2, 'necklace');
   } else if (e.target.classList.contains('bowtie')) {
-    console.log(2);
     outfit.addGarment(2, 'bowtie');
   } else if (e.target.classList.contains('watch')) {
-    console.log(3);
     outfit.addGarment(2, 'watch');
   } else if (e.target.classList.contains('monocle')) {
-    console.log(4);
     outfit.addGarment(2, 'monocle');
   } else if (e.target.classList.contains('earrings')) {
-    console.log(4);
     outfit.addGarment(2, 'earrings');
     console.log('Are accessories being added?');
   }
